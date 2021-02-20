2e2e2a027a18aa98a20754005ecb57f4b483b76f
trixdragon1 <49264102+trixdragon1@users.noreply.github.com>
2021-02-20 14:45:51 -0500

    Add files via upload

--- login-page/src/components/App.js
+++ login-page/src/components/App.js
@@ -1,29 +1,40 @@
 //import React from "react";
 import Login from "./Login";
 import Signup from "./Signup";
-import UIContainer from "./UIContainer";
+import ForgotPassword from "./ForgotPassword";
+import securityQuestions from "./SecurityQuestions";
+import Navbar from "../components/Navbar";
+//import Home from "../components/pages/Home"
+//import UIContainer from "./UIContainer";
 //import RouTer from "./RouTer";
 //import { Container } from "react-bootstrap";
 //import { AuthProvider } from "../contexts/AuthContext";
 
 //import ReactDOM from "react-dom";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import ForgotPassword from "./ForgotPassword";
-import securityQuestions from "./SecurityQuestions";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 //Routing
 function App() {
   return (
-    <div style={{ backgroundColor: "#2c2f33" }}>
-      <BrowserRouter>
+    <>
+      <Router>
         <Switch>
           <Route exact path="/" component={Login}></Route>
           <Route exact path="/signup" component={Signup}></Route>
           <Route exact path="/forgotpassword" component={ForgotPassword}></Route>
           <Route exact path="/securityquestions" component={securityQuestions}></Route>
+          </Switch>
+      </Router>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/home"></Route>
+          <Route exact path="/profile"></Route>
+          <Route exact path="/admin"></Route>
+          <Route exact path="/messages"></Route>
         </Switch>
-      </BrowserRouter>
-    </div>
+      </Router>
+    </>
   );
 }
 
