a98d741687079738e88dc79eab6ee4a49362dccf
trixdragon1 <49264102+trixdragon1@users.noreply.github.com>
2021-02-25 01:50:28 -0500

    Add files via upload

---
+++ accounting-app/src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/home" className="navbar-logo">
+            <img className="img" src={"/logo.png"} alt="logo"
+                  style={{ scale: "69%" }}
+            ></img>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/home" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/profile" className="nav-links">
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/admin" className="nav-links">
+                Admin
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/manager" className="nav-links">
+                Manager
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/user" className="nav-links">
+                User
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links">
+                Sign Out
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
