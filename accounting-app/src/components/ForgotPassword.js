8bd5adb8f618b964383378a3deab8e15196a9f7b
trixdragon1 <49264102+trixdragon1@users.noreply.github.com>
2021-02-25 01:55:19 -0500

    Add files via upload

---
+++ accounting-app/src/components/ForgotPassword.js
@@ -0,0 +1,79 @@
+import logo from "./logo.png";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+
+export default function Signup() {
+  var test = document.getElementById("address");
+  if (test) {
+    console.log(test);
+  }
+
+  return (
+    <div>
+      <Image
+        className="img"
+        src={logo}
+        alt="logo"
+        style={{
+          textAlign: "Center",
+          justifyContent: "Center",
+          display: "block",
+          alignItems: "center",
+          margin: "Auto",
+        }}
+      ></Image>
+      <Grid
+        className="d-flex align-items-center justify-content-center"
+        style={{ height: "1vh" }}
+        textAlign="center"
+        verticalAlign="middle"
+        maxHeight="100vh"
+      >
+        <Grid.Column style={{ maxWidth: 500 }}>
+          <Header as="h2" color="black" textAlign="center">
+            <h2 className="text-center mb-4">Forgot Password</h2>
+          </Header>
+
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                required
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                required
+              />
+
+              <Button color="blue" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+
+          <div className="w-100 text-center mt-2">
+            <Link to="/signup"> Need an Account? Signup</Link>
+          </div>
+          <div className="w-100 text-center mt-2">
+            <Link to="/">Already have an account? Login?</Link>
+          </div>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+}
