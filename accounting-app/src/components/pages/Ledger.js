1b35acef78a759e31b876937db26744d063e165c
K-N-M-C <77809946+K-N-M-C@users.noreply.github.com>
2021-03-25 23:34:21 -0400

    Add files via upload

--- accounting-app/src/components/pages/Ledger.js
+++ accounting-app/src/components/pages/Ledger.js
@@ -1,12 +1,204 @@
 import React from "react";
-import "../App.css";
+import { MDBDataTableV5 } from "mdbreact";
+import { Icon, Button, Header, Modal } from "semantic-ui-react";
+import AddJournal from "../AddJournal";
 
-function Home () {
+export default function WithFilter() {
+  const [datatable, setDatatable] = React.useState({
+    columns: [
+      {
+        label: "Entry ID",
+        field: "entryID",
+        textAlign: "center",
+        width: 100,
+
+        attributes: {
+          "aria-controls": "DataTable",
+          "aria-label": "Name",
+        },
+      },
+      {
+        label: "Entry Date",
+        field: "entryDate",
+        width: 200,
+        textAlign: "center",
+      },
+      {
+        label: "Status",
+        field: "approvalStatus",
+        width: 200,
+      },
+      {
+        label: "Transaction Details",
+        field: "transactionDetails",
+        sort: "disabled",
+        textAlign: "center",
+        width: 1000,
+      },
+      {
+        label: "Edit",
+        field: "edit",
+        sort: "disabled",
+        textAlign: "center",
+        width: 150,
+      },
+      {
+        label: "Delete",
+        field: "delete",
+        sort: "disabled",
+        textAlign: "center",
+        width: 150,
+      },
+    ],
+    rows: [
+      {
+        entryID: "0001",
+        entryDate: "3/20/2021",
+        approvalStatus: "Approved",
+        transactionDetails: 
+          <table>
+            <thead>
+              <tr>
+                  <th>Account Name</th>
+                  <th>Credit</th>
+                  <th>Debit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>Supply Expense</td>
+                  <td>$500</td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td>Cash</td>
+                  <td></td>
+                  <td>$600</td>
+              </tr>
+            </tbody>
+          </table>,
+        edit: <Button icon="edit outline" color="blue" />,
+        delete: (
+          <Button icon="trash alternate outline" width="100px" color="black" />
+        ),
+      },
+      {
+        entryID: "0002",
+        entryDate: "3/21/2021",
+        approvalStatus: "Approved",
+        transactionDetails: 
+          <table>
+            <thead>
+              <tr>
+                  <th>Account Name</th>
+                  <th>Credit</th>
+                  <th>Debit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>Checking</td>
+                  <td>$100</td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td>Design Income</td>
+                  <td></td>
+                  <td>$100</td>
+              </tr>
+            </tbody>
+          </table>,
+        edit: <Button icon="edit outline" color="blue" />,
+        delete: (
+          <Button icon="trash alternate outline" width="100px" color="black" />
+        ),
+      },
+      {
+        entryID: "88903",
+        entryDate: "3/22/2021",
+        approvalStatus: "Approved",
+        transactionDetails: 
+          <table>
+            <thead>
+              <tr>
+                  <th>Account Name</th>
+                  <th>Credit</th>
+                  <th>Debit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>Business account</td>
+                  <td>$300</td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td>Office Supplies</td>
+                  <td></td>
+                  <td>$300</td>
+              </tr>
+            </tbody>
+          </table>,
+        edit: <Button icon="edit outline" color="blue" />,
+        delete: (
+          <Button icon="trash alternate outline" width="100px" color="black" />
+        ),
+      },
+      {
+        entryID: "0004",
+        entryDate: "3/23/2021",
+        approvalStatus: "Pending",
+        transactionDetails: 
+          <table>
+            <thead>
+              <tr>
+                  <th>Account Name</th>
+                  <th>Credit</th>
+                  <th>Debit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>Cash</td>
+                  <td>$200</td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td>Service Revenue</td>
+                  <td></td>
+                  <td>$200</td>
+              </tr>
+            </tbody>
+          </table>,
+        edit: <Button icon="edit outline" color="blue" />,
+        delete: (
+          <Button icon="trash alternate outline" width="100px" color="black" />
+        ),
+      },
+    ],
+  });
+
   return (
-    <>
-      <p>Test</p>
-    </>
-  )
-}
+    <div
+      style={{
+        marginTop: "5%",
+        marginLeft: "5px",
+        marginRight: "5px",
+        textAlign: "center",
+      }}
+    >
+      <Icon name="archive" size="huge" color="grey" />
+      <h1>Supplies Expense Ledger</h1>
 
-export default Home;
+      
+      <MDBDataTableV5
+        hover
+        data={datatable}
+        filter="office"
+        proSelect
+        searchTop
+        searchBottom={false}
+      />
+    </div>
+  );
+}
